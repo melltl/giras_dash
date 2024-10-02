bebc3b48a7241d39a0f2651e87441d22d0e6939c
Mel <meltavares1063@gmail.com>
2024-10-01 23:40:00 -0300

    adicionando arquivo

--- README.md
+++ README.md
@@ -1,19 +1,35 @@
+# Análise de Dados do Spotify 2023
+
+Este projeto é uma aplicação Streamlit que realiza uma análise abrangente dos dados do Spotify para o ano de 2023. A aplicação oferece visualizações interativas e insights sobre popularidade de artistas, impacto de playlists, tendências temporais, características musicais e comparações entre artistas solo e colaborativos.
+
+
 ### Base de dados
-Vendas da amazon 
+Link da base:https://www.kaggle.com/datasets/nelgiriyewithana/top-spotify-songs-2023 
 - Colunas:
-  - Category: Tipo de produto. (String)
-  - Size: Tamanho do produto. (String)
-  - Date: Data da venda. (Data)
-  - Status: Status da venda. (String)
-  - Fulfilment: Método de entrega. (String)
-  - Style: Estilo do produto. (String)
-  - SKU: Unidade de Manutenção de Estoque. (String)
-  - ASIN: Número de Identificação Padrão da Amazon. (String)
-  - Courier Status: Status do serviço de entrega. (String)
-  - Qty: Quantidade do produto. (Inteiro)
-  - Amount: Valor da venda. (Float)
-  - B2B: Venda de empresa para empresa. (Booleano)
-  - Currency: A moeda utilizada na venda. (String)
+* **track_name**: Nome da música
+* **artist(s)_name**: Nome do(s) artista(s) da música
+* **artist_count**: Número de artistas que contribuíram para a música
+* **released_year**: Ano em que a música foi lançada
+* **released_month**: Mês em que a música foi lançada
+* **released_day**: Dia do mês em que a música foi lançada
+* **in_spotify_playlists**: Número de playlists do Spotify em que a música está incluída
+* **in_spotify_charts**: Presença e posição da música nas paradas do Spotify
+* **streams**: Número total de reproduções no Spotify
+* **in_apple_playlists**: Número de playlists do Apple Music em que a música está incluída
+* **in_apple_charts**: Presença e posição da música nas paradas do Apple Music
+* **in_deezer_playlists**: Número de playlists do Deezer em que a música está incluída
+* **in_deezer_charts**: Presença e posição da música nas paradas do Deezer
+* **in_shazam_charts**: Presença e posição da música nas paradas do Shazam
+* **bpm**: Batidas por minuto, uma medida do tempo da música
+* **key**: Tonalidade da música
+* **mode**: Modo da música (maior ou menor)
+* **danceability_%**: Porcentagem que indica o quão adequada a música é para dançar
+* **valence_%**: Positividade do conteúdo musical da música
+* **energy_%**: Nível percebido de energia da música
+* **acousticness_%**: Quantidade de som acústico na música
+* **instrumentalness_%**: Quantidade de conteúdo instrumental na música
+* **liveness_%**: Presença de elementos de performance ao vivo
+* **speechiness_%**: Quantidade de palavras faladas na música
 
 
 
@@ -21,30 +37,74 @@ Vendas da amazon
 
 
 
-### Tratamento de dados
+
+## Tratamento de Dados
+
+O tratamento de dados é uma etapa crucial neste projeto. Aqui estão os principais passos:
+
+1. **Encoding dos nomes dos artistas:**
+   ```python
+   df['artist(s)_name'] = df['artist(s)_name'].str.encode('utf-8', errors='ignore').str.decode('utf-8')
+   ```
+   Garante a correta codificação dos nomes dos artistas em UTF-8.
+
+2. **Remoção de valores anômalos:**
+   ```python
+   df = df[df['streams'] != valor_anomalo]
+   ```
+   Remove uma linha específica com valor anômalo na coluna 'streams'.
+
+3. **Conversão de colunas para tipo numérico:**
+   ```python
+   numeric_columns = ['streams', 'in_deezer_playlists', 'in_shazam_charts']
+   for col in numeric_columns:
+       df[col] = pd.to_numeric(df[col], errors='coerce')
+   ```
+   Assegura que colunas específicas sejam tratadas como números.
+
+4. **Tratamento de valores ausentes:**
+   ```python
+   df['key'] = df['key'].fillna('Unknown')
+   ```
+   Preenche valores ausentes na coluna 'key'.
+
+5. **Normalização de colunas de porcentagem:**
+   ```python
+   percentage_columns = ['danceability_%', 'valence_%', 'energy_%', 'acousticness_%',
+                         'instrumentalness_%', 'liveness_%', 'speechiness_%']
+   for col in percentage_columns:
+       df[col] = df[col] / 100
+       df = df.rename(columns={col: col.replace('_%', '')})
+   ```
+   Converte porcentagens para escala de 0 a 1 e renomeia as colunas.
+
+6. **Criação de coluna de data de lançamento:**
+   ```python
+   df['release_date'] = pd.to_datetime(df[['released_year', 'released_month', 'released_day']].astype(str).agg('-'.join, axis=1))
+   ```
+   Combina ano, mês e dia em uma única coluna de data.
+
+7. **Remoção de linhas com valores ausentes:**
+   ```python
+   df = df.dropna()
+   ```
+   Elimina linhas com valores ausentes após todos os tratamentos.
+
+## Funcionalidades
+
+A aplicação inclui várias análises interativas:
+
+1. **Análise de popularidade de artistas:** Visualiza os artistas mais populares com base no número de streams.
+
+2. **Impacto da presença em playlists nos streams:** Analisa a relação entre a presença em playlists e o número de streams.
+
+3. **Análise temporal de streams e playlists:** Examina tendências anuais e mensais de streams e presença em playlists.
+
+4. **Análise de características musicais e presença nos charts:** Explora como diferentes características musicais afetam a presença em charts.
+
+5. **Comparação entre artistas solo e colaborativos:** Compara o desempenho de artistas solo versus colaborações.
+
+6. **Correlações entre características musicais e streams:** Visualiza correlações entre diferentes atributos musicais e popularidade.
 
 
-  ```py
-  # 1. Substituir valores NaN na coluna 'Courier Status' por 'unknown'
-  df['Courier Status'].fillna('unknown', inplace=True)
   
-  # 2. Substituir valores NaN na coluna 'currency' por 'unknown'
-  df['currency'].fillna('unknown', inplace=True)
-  
-  # 3. Substituir valores NaN na coluna 'Amount' pela média da categoria
-  # Supondo que a coluna 'categoria' é a coluna pela qual será calculada a média
-  df['Amount'] = df.groupby('Category')['Amount'].transform(lambda x: x.fillna(x.mean()))
-  
-  # 4. Transformar '#promotion_id' em True (quando houver ID) e False (quando não houver ID)
-  df['promotion-ids'] = df['promotion-ids'].notna()
-  
-  # 5. Dropar as colunas 'fufilled by' e 'unnamed'
-  df.drop(columns=['fulfilled-by', 'Unnamed: 22','SKU'], inplace=True)
-
-  # 6. Mudar tipo de 'object' para datetime
-  df['Date']=pd.to_datetime(df['Date']
-
-  #7. Renomear coluna para facilitar na hora da análise
-  df.rename(columns={'Sales Channel ': 'Sales Channel'},inplace=True)
-
-```
--- app.py
+++ app.py
@@ -1,182 +1,349 @@
 import streamlit as st
 import pandas as pd
-import seaborn as sns
 import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 import numpy as np
-from datetime import datetime, timedelta
-import folium
-from streamlit_folium import folium_static
-from geopy.geocoders import Nominatim
-from geopy.exc import GeocoderTimedOut, GeocoderUnavailable
 
-# Configuração da página Streamlit
-st.set_page_config(layout="wide")
+df = pd.read_csv('spotify-2023.csv', encoding='ISO-8859-1')
 
-# Paleta de cores personalizada
-color_palette = ['#EDAFB8', '#F7E1D7', '#DEDBD2', '#d62728', '#B0C4B1', '#4A5759', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
 
-# Carregamento e pré-processamento de dados
-@st.cache_data
-def carregar_e_preprocessar_dados():
-    df = pd.read_csv('Amazon Sale Report.csv')
-    df.rename(columns={'Sales Channel ': 'Sales Channel'}, inplace=True)
-    df['Courier Status'].fillna('Unknown', inplace=True)
-    df['currency'].fillna('Unknown', inplace=True)
-    df['Amount'] = df.groupby('Category')['Amount'].transform(lambda x: x.fillna(x.mean()))
-    df['promotion-ids'] = df['promotion-ids'].notna()
-    df.drop(columns=['fulfilled-by', 'Unnamed: 22', 'SKU'], inplace=True)
-    df['Date'] = pd.to_datetime(df['Date'])
+df['artist(s)_name'] = df['artist(s)_name'].str.encode(
+    'utf-8', errors='ignore').str.decode('utf-8')
+
+valor_anomalo = 'BPM110KeyAModeMajorDanceability53Valence75Energy69Acousticness7Instrumentalness0Liveness17Speechiness3'
+
+# Removendo a linha que contém o valor anômalo na coluna 'streams'
+df = df[df['streams'] != valor_anomalo]
+
+
+def treat_data(df):
+    numeric_columns = ['streams', 'in_deezer_playlists', 'in_shazam_charts']
+    for col in numeric_columns:
+        df[col] = pd.to_numeric(df[col], errors='coerce')
+
+    df['key'] = df['key'].fillna('Unknown')
+
+    percentage_columns = ['danceability_%', 'valence_%', 'energy_%', 'acousticness_%',
+                          'instrumentalness_%', 'liveness_%', 'speechiness_%']
+    for col in percentage_columns:
+        df[col] = df[col] / 100
+        df = df.rename(columns={col: col.replace('_%', '')})
+
+    df['release_date'] = pd.to_datetime(
+        df[['released_year', 'released_month', 'released_day']].astype(str).agg('-'.join, axis=1))
+
+    df = df.dropna()
+
     return df
 
-df = carregar_e_preprocessar_dados()
 
-# Funções utilitárias
-def criar_grafico_barras(data, x, y, titulo, xlabel, ylabel, rotation=45):
-    fig, ax = plt.subplots(figsize=(10, 6))
-    sns.barplot(data=data, x=x, y=y, palette=color_palette, ax=ax)
-    ax.set_title(titulo, fontsize=16)
-    ax.set_xlabel(xlabel, fontsize=12)
-    ax.set_ylabel(ylabel, fontsize=12)
-    plt.xticks(rotation=rotation)
-    plt.tight_layout()
-    return fig
+# Aplicando o tratamento
+df = treat_data(df)
 
-@st.cache_data
-def obter_coordenadas(cidade):
-    geolocator = Nominatim(user_agent="meu_agente")
-    try:
-        location = geolocator.geocode(cidade)
-        if location:
-            return (location.latitude, location.longitude)
-    except (GeocoderTimedOut, GeocoderUnavailable):
-        pass
-    return None
+###################################################
 
-# Dashboard principal
-st.title("Dashboard de Vendas da Amazon")
+st.title('Análise de popularidade de artistas')
 
-# Vendas por Mês
-st.header("Análise de Vendas Mensais")
-vendas_por_mes = df.groupby(df['Date'].dt.strftime('%B'))['Amount'].sum().reset_index()
-vendas_por_mes['Date'] = pd.to_datetime(vendas_por_mes['Date'], format='%B')
-vendas_por_mes = vendas_por_mes.sort_values('Date')
-vendas_por_mes.columns = ['Mês', 'Total de Vendas']
+st.sidebar.header('Controles - Análise de popularidade de artistas')
 
-col1, col2 = st.columns(2)
+# Slider para selecionar o número de artistas top
+num_artistas = st.sidebar.slider('Número de artistas top', 5, 50, 10)
 
-with col1:
-    fig_month = criar_grafico_barras(vendas_por_mes, 'Mês', 'Total de Vendas',
-                                     'Total de Vendas por Mês', 'Mês', 'Total de Vendas (R$)')
-    st.pyplot(fig_month)
+ordem_invertida = st.sidebar.checkbox(
+    'Mostrar artistas mais populares primeiro', value=True)
 
-with col2:
-    st.subheader("Dados de Vendas Mensais")
-    st.dataframe(vendas_por_mes.style.format({'Total de Vendas': 'R${:,.2f}'}))
+tipo_stream = st.sidebar.radio('Tipo de streams', ['Total', 'Médio'])
 
-# Análise por Categoria
-st.header("Análise de Categoria de Produtos")
-vendas_categoria = df.groupby('Category').agg({'Qty': 'sum', 'Amount': 'sum'}).reset_index()
+if tipo_stream == 'Total':
+    df_grouped = df.groupby('artist(s)_name')['streams'].sum().reset_index()
+else:
+    df_grouped = df.groupby('artist(s)_name')['streams'].mean().reset_index()
 
-col1, col2 = st.columns(2)
+df_sorted = df_grouped.sort_values(
+    'streams', ascending=not ordem_invertida).head(num_artistas)
 
-with col1:
-    fig_qty = criar_grafico_barras(vendas_categoria, 'Qty', 'Category',
-                                   'Quantidade Vendida por Categoria', 'Quantidade', 'Categoria')
-    st.pyplot(fig_qty)
+fig = px.bar(df_sorted,
+             x='streams',
+             y='artist(s)_name',
+             orientation='h',
+             title=f'Top {num_artistas} Artistas por Número de Streams {tipo_stream}s',
+             labels={'streams': 'Número de Streams', 'artist(s)_name': 'Artista'})
 
-with col2:
-    fig_amount = criar_grafico_barras(vendas_categoria, 'Amount', 'Category',
-                                      'Total de Vendas por Categoria', 'Total de Vendas (R$)', 'Categoria')
-    st.pyplot(fig_amount)
+fig.update_layout(yaxis={'categoryorder': 'total ascending'})
 
-st.subheader("Dados de Vendas por Categoria")
-st.dataframe(vendas_categoria.style.format({'Amount': 'R${:,.2f}'}))
+st.plotly_chart(fig)
 
-# Desempenho do Correio
-st.header("Análise de Desempenho do Correio")
-status_correio = df['Courier Status'].value_counts().reset_index()
-status_correio.columns = ['Status do Correio', 'Frequência']
+# Adicionar algumas estatísticas
+st.write('Estatísticas:')
+st.write(
+    f"Total de streams para estes artistas: {df_sorted['streams'].sum():,}")
+st.write(f"Média de streams por artista: {df_sorted['streams'].mean():,.2f}")
 
-fig_courier = criar_grafico_barras(status_correio, 'Frequência', 'Status do Correio',
-                                   'Desempenho de Entrega por Status do Correio', 'Frequência', 'Status do Correio')
-st.pyplot(fig_courier)
+############################################
 
-st.subheader("Dados de Status do Correio")
-st.dataframe(status_correio)
+st.title('Impacto da Presença em Playlists nos Streams por Plataforma')
 
-# Vendas por Localização
-st.header("Principais Locais de Venda")
+fig = go.Figure()
 
-col1, col2 = st.columns(2)
+platforms = ['spotify', 'apple', 'deezer']
+colors = ['blue', 'red', 'green']
 
-with col1:
-    st.subheader("Top 10 Cidades por Vendas")
-    vendas_por_cidade = df.groupby('ship-city')['Amount'].sum().sort_values(ascending=False).head(10)
-    fig_cities = criar_grafico_barras(vendas_por_cidade.reset_index(), 'Amount', 'ship-city',
-                                      'Top 10 Cidades por Vendas', 'Total de Vendas (R$)', 'Cidade')
-    st.pyplot(fig_cities)
+for platform, color in zip(platforms, colors):
+    fig.add_trace(go.Scatter(
+        x=df[f'in_{platform}_playlists'],
+        y=df['streams'],
+        mode='markers',
+        name=platform.capitalize(),
+        marker=dict(color=color, opacity=0.3),
+    ))
 
-with col2:
-    st.subheader("Top 10 Estados por Vendas")
-    vendas_por_estado = df.groupby('ship-state')['Amount'].sum().sort_values(ascending=False).head(10)
-    fig_states = criar_grafico_barras(vendas_por_estado.reset_index(), 'Amount', 'ship-state',
-                                      'Top 10 Estados por Vendas', 'Total de Vendas (R$)', 'Estado')
-    st.pyplot(fig_states)
+    x = df[f'in_{platform}_playlists']
+    y = df['streams']
+    z = np.polyfit(x, y, 1)
+    p = np.poly1d(z)
+    fig.add_trace(go.Scatter(
+        x=x,
+        y=p(x),
+        mode='lines',
+        name=f'{platform.capitalize()} (Regressão)',
+        line=dict(color=color)
+    ))
 
-# Mapa de Calor de Categorias de Produtos e Estados
-st.header("Relação entre Categorias de Produtos e Estados")
-categoria_regiao = df.groupby(['Category', 'ship-state'])['Amount'].sum().unstack()
-fig, ax = plt.subplots(figsize=(12, 8))
-sns.heatmap(categoria_regiao, cmap='YlOrRd', annot=False, ax=ax)
-ax.set_title('Relação entre Categorias de Produtos e Estados', fontsize=16)
-ax.set_xlabel('Estados', fontsize=12)
-ax.set_ylabel('Categorias', fontsize=12)
-plt.tight_layout()
-st.pyplot(fig)
-
-# Impacto das Promoções
-st.header("Impacto das Promoções nas Vendas")
-impacto_promo = df.groupby('promotion-ids')['Amount'].sum()
-fig, ax = plt.subplots(figsize=(10, 6))
-impacto_promo.plot(kind='pie', ax=ax, autopct='%1.1f%%', startangle=90, colors=color_palette[:2])
-ax.set_title('Impacto das Promoções nas Vendas', fontsize=16)
-ax.set_ylabel('')  # Remove o rótulo do eixo y
-plt.axis('equal')
-st.pyplot(fig)
-
-# Vendas por Data
-st.header("Análise de Vendas Diárias")
-data_min = df['Date'].min().date()
-data_max = df['Date'].max().date()
-data_selecionada = st.date_input(
-    "Selecione uma data",
-    min_value=data_min,
-    max_value=data_max,
-    value=data_min
+fig.update_layout(
+    title='Impacto da Presença em Playlists nos Streams por Plataforma',
+    xaxis_title='Número de Playlists',
+    yaxis_title='Número de Streams',
+    legend_title='Plataforma',
+    height=600,
+    width=900
 )
 
-dados_selecionados = df[df['Date'].dt.date == data_selecionada]
-qtd_vendida = dados_selecionados['Qty'].sum()
-total_vendas = dados_selecionados['Amount'].sum()
+st.plotly_chart(fig)
 
-col1, col2 = st.columns(2)
-with col1:
-    st.metric(label=f"Produtos Vendidos em {data_selecionada.strftime('%d/%m/%Y')}", value=qtd_vendida)
-with col2:
-    st.metric(label=f"Total de Vendas em {data_selecionada.strftime('%d/%m/%Y')}", value=f"R${total_vendas:,.2f}")
+##########################################################
 
-# Mapa de Vendas
-st.subheader(f"Mapa de Vendas para {data_selecionada.strftime('%d/%m/%Y')} (Top 150 Cidades)")
-m = folium.Map(location=[0, 0], zoom_start=2, tiles="cartodbpositron")
+st.title('Análise temporal de streams e playlists')
 
-top_cidades = dados_selecionados['ship-city'].value_counts().nlargest(150).index
+st.sidebar.header('Controles - Análise temporal de streams e playlists')
 
-for cidade in top_cidades:
-    coordenadas = obter_coordenadas(cidade)
-    if coordenadas:
-        folium.Marker(
-            location=coordenadas,
-            popup=f"Cidade: {cidade}"
-        ).add_to(m)
+# Checkbox para escolher entre análise anual ou mensal
+analysis_type = st.sidebar.radio(
+    "Escolha o tipo de análise:", ('Anual', 'Mensal'))
 
-folium_static(m)
+
+def create_plot(data, x_col, title, x_title, x_tickvals=None, x_ticktext=None):
+    fig = make_subplots(specs=[[{"secondary_y": True}]])
+
+    fig.add_trace(
+        go.Scatter(x=data[x_col], y=data['streams'], name="Streams"),
+        secondary_y=False,
+    )
+
+    for platform in ['spotify', 'apple', 'deezer']:
+        fig.add_trace(
+            go.Scatter(
+                x=data[x_col], y=data[f'in_{platform}_playlists'], name=f"{platform.capitalize()} Playlists"),
+            secondary_y=True,
+        )
+
+    fig.update_layout(
+        title_text=title,
+        xaxis_title=x_title,
+    )
+
+    if x_tickvals and x_ticktext:
+        fig.update_xaxes(tickmode='array', tickvals=x_tickvals,
+                         ticktext=x_ticktext)
+
+    fig.update_yaxes(title_text="Média de Streams", secondary_y=False)
+    fig.update_yaxes(
+        title_text="Média de Presença em Playlists", secondary_y=True)
+
+    return fig
+
+
+if analysis_type == 'Anual':
+    yearly_data = df.groupby(df['release_date'].dt.year).agg({
+        'streams': 'mean',
+        'in_spotify_playlists': 'mean',
+        'in_apple_playlists': 'mean',
+        'in_deezer_playlists': 'mean'
+    }).reset_index()
+
+    fig = create_plot(yearly_data, 'release_date',
+                      "Tendência Anual: Streams e Presença em Playlists", "Ano de Lançamento")
+    st.plotly_chart(fig)
+
+    st.write("Correlação entre ano de lançamento e streams:",
+             df['released_year'].corr(df['streams']))
+
+else:
+    monthly_data = df.groupby(df['release_date'].dt.month).agg({
+        'streams': 'mean',
+        'in_spotify_playlists': 'mean',
+        'in_apple_playlists': 'mean',
+        'in_deezer_playlists': 'mean'
+    }).reset_index()
+
+    months = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun',
+              'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez']
+    fig = create_plot(monthly_data, 'release_date', "Sazonalidade Mensal: Streams e Presença em Playlists",
+                      "Mês de Lançamento", x_tickvals=list(range(1, 13)), x_ticktext=months)
+    st.plotly_chart(fig)
+
+    monthly_corr = df.groupby(df['release_date'].dt.month)[
+        'streams'].mean().corr(pd.Series(range(1, 13)))
+    st.write("Correlação entre mês de lançamento e streams:", monthly_corr)
+
+##########################################
+
+st.title('Análise de características musicais e presença nos charts')
+
+st.sidebar.header('Controles - Análise de características musicais e presença nos charts')
+
+# Seleção da característica musical
+feature_options = ['danceability', 'energy', 'acousticness', 'instrumentalness', 'liveness', 'speechiness', 'valence']
+selected_feature = st.sidebar.selectbox('Escolha a característica musical:', feature_options)
+
+def create_categories(df, feature):
+    return pd.cut(df[feature], 
+                  bins=[0, 0.2, 0.4, 0.6, 0.8, 1.0],
+                  labels=['Muito Baixa', 'Baixa', 'Média', 'Alta', 'Muito Alta'])
+
+df['feature_category'] = create_categories(df, selected_feature)
+
+# Calcular médias por categoria
+feature_stats = df.groupby('feature_category').agg({
+    'in_spotify_charts': 'mean',
+    'in_apple_charts': 'mean',
+    'in_deezer_charts': 'mean',
+    'in_shazam_charts': 'mean',
+    'streams': 'mean'
+}).reset_index()
+
+fig = go.Figure()
+
+for chart in ['in_spotify_charts', 'in_apple_charts', 'in_deezer_charts', 'in_shazam_charts']:
+    fig.add_trace(go.Bar(
+        x=feature_stats['feature_category'],
+        y=feature_stats[chart],
+        name=chart.replace('in_', '').replace('_charts', '').capitalize()
+    ))
+
+fig.update_layout(
+    title=f'Presença nos Charts por Categoria de {selected_feature.replace("_%", "")}',
+    xaxis_title=f'Categoria de {selected_feature.replace("_%", "")}',
+    yaxis_title='Média de Presença nos Charts',
+    barmode='group'
+)
+
+st.plotly_chart(fig)
+
+# Adicionar estatísticas
+st.subheader('Estatísticas por Categoria')
+st.write(feature_stats)
+
+#################################################
+
+st.title('Análise comparativa: artistas solo vs. colaborativos')
+
+df['is_collaborative'] = df['artist_count'] > 1
+
+# Calcular médias para métricas de sucesso
+success_metrics = ['streams', 'in_spotify_playlists', 'in_apple_playlists', 'in_deezer_playlists']
+avg_metrics = df.groupby('is_collaborative')[success_metrics].mean().reset_index()
+
+fig = make_subplots(rows=2, cols=2, subplot_titles=("Streams", "Spotify Playlists", "Apple Music Playlists", "Deezer Playlists"))
+
+for i, metric in enumerate(success_metrics):
+    row = i // 2 + 1
+    col = i % 2 + 1
+    
+    fig.add_trace(
+        go.Bar(x=['Solo', 'Collaborative'], 
+               y=avg_metrics[metric],
+               text=avg_metrics[metric].round(0),
+               textposition='auto',
+               name=metric),
+        row=row, col=col
+    )
+
+fig.update_layout(height=700, width=800, title_text="Comparação de Métricas de Sucesso: Artistas Solo vs. Colaborativos")
+fig.update_traces(texttemplate='%{text:.2s}', textposition='outside')
+fig.update_layout(uniformtext_minsize=8, uniformtext_mode='hide')
+
+st.plotly_chart(fig)
+
+# Calcular e mostrar percentuais de aumento/diminuição
+st.subheader('Impacto Percentual das Colaborações')
+for metric in success_metrics:
+    solo_value = avg_metrics.loc[avg_metrics['is_collaborative'] == False, metric].values[0]
+    collab_value = avg_metrics.loc[avg_metrics['is_collaborative'] == True, metric].values[0]
+    percent_change = ((collab_value - solo_value) / solo_value) * 100
+    st.write(f"{metric}: {percent_change:.2f}% {'aumento' if percent_change > 0 else 'diminuição'} para colaborações")
+
+##########################################################
+
+st.title('Correlações entre características musicais e streams')
+
+st.sidebar.header('Controles - Correlações entre características musicais e streams')
+
+# Seleção de características
+all_features = ['danceability', 'valence', 'energy', 'acousticness', 'streams']
+selected_features = st.sidebar.multiselect(
+    'Escolha as características para análise:',
+    all_features,
+    default=all_features
+)
+
+# Calcular a matriz de correlação
+correlation_matrix = df[selected_features].corr()
+
+fig = go.Figure(data=go.Heatmap(
+                    z=correlation_matrix.values,
+                    x=correlation_matrix.columns,
+                    y=correlation_matrix.columns,
+                    hoverongaps = False,
+                    texttemplate="%{z:.2f}",
+                    colorscale='RdBu'))
+
+fig.update_layout(
+    title='Mapa de Calor de Correlações',
+    xaxis_title='Características',
+    yaxis_title='Características'
+)
+
+st.plotly_chart(fig)
+
+# Gráfico de dispersão
+st.subheader('Gráfico de Dispersão')
+x_axis = st.selectbox('Escolha a característica para o eixo X:', selected_features)
+y_axis = st.selectbox('Escolha a característica para o eixo Y:', 
+                      [feat for feat in selected_features if feat != x_axis], 
+                      index=1)
+
+fig_scatter = go.Figure(data=go.Scatter(
+    x=df[x_axis],
+    y=df[y_axis],
+    mode='markers',
+    marker=dict(
+        size=5,
+        color=df['streams'],
+        colorscale='Viridis',
+        showscale=True
+    ),
+    text=df['track_name'] + ' - ' + df['artist(s)_name'],
+    hoverinfo='text'
+))
+
+fig_scatter.update_layout(
+    title=f'{x_axis.capitalize()} vs {y_axis.capitalize()}',
+    xaxis_title=x_axis.capitalize(),
+    yaxis_title=y_axis.capitalize(),
+    coloraxis_colorbar=dict(title="Streams")
+)
+
+st.plotly_chart(fig_scatter)
